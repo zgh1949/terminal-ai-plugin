e06819cd65e13281200aadedea4cdf52b6890f37
zgh <guanghua.zhu@tigermedgrp.com>
2026-03-05 14:51:51 +0800

    兼容性

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,18 +1,18 @@
 plugins {
     id("java")
     id("org.jetbrains.kotlin.jvm") version "1.9.20"
-    id("org.jetbrains.intellij") version "1.16.1"
+    id("org.jetbrains.intellij") version "1.17.4"
 }
 
 group = "com.terminalai"
-version = "0.0.2"
+version = "0.0.3"
 
 repositories {
     mavenCentral()
 }
 
 intellij {
-    version.set("2023.3")
+    version.set("2024.2")
     type.set("IC")
     plugins.set(listOf())
 }
