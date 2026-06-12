946224fa0634817f7f90749b9a63966e3d34bf77
zgh <guanghua.zhu@tigermedgrp.com>
2026-06-12 13:50:47 +0800

    chore(build): 更新插件版本并移除构建版本限制
    - 将插件版本从 0.0.3 更新到 0.4
    - 移除 build.gradle.kts 中的 sinceBuild 设置
    - 从 plugin.xml 中移除 since-build 属性以支持更广泛的 IDE 版本

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "com.terminalai"
-version = "0.0.3"
+version = "0.0.4"
 
 repositories {
     mavenCentral()
@@ -27,6 +27,5 @@ tasks {
     }
 
     patchPluginXml {
-        sinceBuild.set("203")
     }
 }
